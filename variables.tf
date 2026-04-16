ea4e652bc9f63b0029c0250cba56f0ec713ce98e
Walae-Ali <98598745+Walae-Ali@users.noreply.github.com>
2026-04-16 16:14:01 +0100

    Initial IaC setup

---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "db_name" {
+  description = "Nom de la base de données PostgreSQL."
+  type        = string
+  default     = "devops_db"
+}
+
+variable "db_user" {
+  description = "Nom d'utilisateur PostgreSQL."
+  type        = string
+  default     = "devops_user"
+}
+
+variable "db_password" {
+  description = "Mot de passe PostgreSQL."
+  type        = string
+  default     = "strongpassword123"
+}
+
+variable "app_port_external" {
+  description = "Port externe pour accéder à l'app (mappé au port 80 interne)."
+  type        = number
+  default     = 8080
+}
